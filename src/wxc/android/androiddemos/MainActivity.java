6fde5bc1f1564dfbc6768cd258d692d8aaf60b91
chenhd <chenhd@m15.cn>
2014-03-25 09:37:42 +0800

    Add:添加QQ列表左滑显示删除按钮

--- src/wxc/android/androiddemos/MainActivity.java
+++ src/wxc/android/androiddemos/MainActivity.java
@@ -1,22 +1,31 @@
 package wxc.android.androiddemos;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.btn_enter_qqlist).setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_enter_qqlist:
+			startActivity(QQListActivity.class);
+			break;
+		default:
+			break;
+		}
 	}
 
+	private void startActivity(Class<? extends Activity> clazz) {
+		startActivity(new Intent(this, clazz));
+	}
 }
---
+++ src/wxc/android/androiddemos/QQListActivity.java
@@ -0,0 +1,72 @@
+package wxc.android.androiddemos;
+
+import wxc.android.androiddemos.widget.QQListView2;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class QQListActivity extends Activity {
+	private QQListView2 mList;
+	private QQListAdapter mAdapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_qqlist);
+		
+		mList = (QQListView2) findViewById(R.id.qqlv);
+		mAdapter = new QQListAdapter(this);
+		mList.setAdapter(mAdapter);
+	}
+	
+	public static class QQListAdapter extends BaseAdapter {
+		private String[] mData = new String[26];
+		private Context mContext;
+		
+		public QQListAdapter(Context ctx) {
+			mContext = ctx;
+			initData();
+		}
+		
+		private void initData() {
+			for (int i = 0; i < 26; i++) {
+				mData[i] = String.valueOf((char) ('A' + i));
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return mData.length;
+		}
+
+		@Override
+		public String getItem(int position) {
+			return mData[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView contentTv = null;
+			if (convertView == null) {
+				convertView = LayoutInflater.from(mContext).inflate(R.layout.item_qqlist, null);
+				contentTv = (TextView) convertView.findViewById(R.id.tv_content);
+				convertView.setTag(contentTv);
+			} else {
+				contentTv = (TextView) convertView.getTag();
+			}
+			contentTv.setText(getItem(position));
+			return convertView;
+		}
+		
+	}
+}
